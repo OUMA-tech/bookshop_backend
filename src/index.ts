fcc30ee799bc65f49181d7e4f1b4c30c87e3cfba
OUMA-tech <tianyfff11@gmail.com>
2025-05-08 14:05:19 +1000

    auth controllers done

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import dotenv from 'dotenv';
 import cors from 'cors';
 import bodyParser from 'body-parser';
 import connectDB from './config/database';
+import morgan from 'morgan';
 
 import authRoutes from './routes/authRoutes';
 // import productRoutes from './routes/productRoutes';
@@ -11,14 +12,26 @@ import authRoutes from './routes/authRoutes';
 
 dotenv.config();
 const app: Application = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT||5000;
 
 // Middleware
 app.use(cors());
 app.use(bodyParser.json());
+app.use(morgan('dev'));
+app.use((req, res, next) => {
+    console.log('--- Incoming Request ---');
+    console.log('Headers:', JSON.stringify(req.headers, null, 2));
+    console.log('Body:', JSON.stringify(req.body, null, 2));
+    console.log('Params:', JSON.stringify(req.params, null, 2));
+    console.log('Query:', JSON.stringify(req.query, null, 2));
+    console.log('------------------------');
+    next();
+  });
 app.use('/api/auth', authRoutes);
 // app.use('/api/products', productRoutes);
 // app.use('/api/cart', cartRoutes);
+
+  
 
 // Database Connection
 connectDB();
